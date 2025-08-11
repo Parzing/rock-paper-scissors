bd99644b38363566bdd400d25079a5480322d000
Parzing <jettae@schroff.com>
2025-08-11 01:12:01 -0700

    Finished this godawful project

--- javascript.js
+++ javascript.js
@@ -1,40 +1,72 @@
-const numberRounds = 5;
+var maxScore = 0;
 const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+let playerWins = 0;
+let computerWins = 0;
 
 function playGame() {
 
     let humanChoice;
     let computerChoice;
 
-    for(let i = 0; i < numberRounds; i++) {
+    while(maxScore < 5) {
         humanChoice = getHumanChoice();
         computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
+        console.log("Player - " + playerWins + "    Computer - " + computerWins);
+        maxScore = Math.max(playerWins, computerWins);
+    }
+
+    if(computerWins == 5) {
+        console.log("Computer wins!");
+    } else {
+        console.log("Human wins!");
     }
 
 }
 
 
 function playRound(humanChoice, computerChoice) {
+    if(humanChoice == computerChoice) {
+        return;
+    }
 
+    if(humanChoice == SCISSORS && computerChoice == PAPER ||
+        humanChoice == PAPER && computerChoice == ROCK ||
+        humanChoice == ROCK && computerChoice == SCISSORS) {
+        playerWins++;
+        return;
+    }
+    else{
+        computerWins++;
+    }
 }
 
 function getHumanChoice() {
     var choice;
     choice = prompt("Enter your choice as \"Rock\", \"Paper\", or \"Scissors\": ");
-    
 
-    while(choiceNotValid()){
+    while(!choiceValid()){
         console.log("Sorry, that was not a valid input.\n");
-        prompt("Enter your choice as \"Rock\", \"Paper\", or \"Scissors\": ");
+        choice = prompt("Enter your choice as \"Rock\", \"Paper\", or \"Scissors\": ");
 
     }
+    
+    if (choice == "Rock") {
+        choice = ROCK;
+    }
+    if (choice == "Paper") {
+        choice = PAPER;
+    }
+    if (choice == "Scissors") {
+        choice = SCISSORS;
+    }
 
+    return choice;
 
-    function choiceNotValid() {
+    function choiceValid() {
         if(choice == "Rock" || choice == "Paper" || choice == "Scissors") {
             return true;
         }
@@ -52,10 +84,6 @@ function getComputerChoice() {
         default:
             return SCISSORS;
     }
-
 }
 
-
-
-
-
+playGame();
